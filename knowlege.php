ad33d8347bed1316c4849df4c4f65662a081e0d0
Mauricio Cantor <topomajor1@hotmail.com>
2015-09-22 16:23:45 -0500

    Sistemas Expertos

---
+++ knowlege.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Prueba </title>
+	<link rel="stylesheet" href="">
+	<script src="js/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+</head>
+	<script>
+		$(document).ready(function(){
+
+			$('#consulta').click(function(){
+				consulta();
+			});
+		});
+
+		function consulta()
+		{
+			$.ajax({
+				url: 'JSON_knowlege/Dutch_Pharmacogenetics_Working_Group_Guideline_for_fluorouracil_and_DPYD.json',
+				//type: 'default GET (Other values: POST)',
+				dataType: 'json',
+				//data: {param1: 'value1'},
+			})
+			.done(function(data, res) {
+				
+				console.log(data);
+				$('#title').html(data.name);
+				$('#title').html(data.relatedDrugs[0]['@id']);
+		
+				$('#summaryHtml').html(data.textHtml);
+				$('#textHtml').html(data.summaryHtml);
+				$('#symbol').html(data.relatedGenes[0].symbol);
+				$('#relatedGenes').html(data.relatedGenes[0].name);
+				$('#relatedDrugs').html(data.relatedDrugs[0].name);
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+		}
+
+	</script>
+<body>
+	<div id="consulta">Mostrar</div>
+	<table  border="1" cellpadding="0">
+		<caption id="title"></caption>
+		<tbody>
+			<tr >
+				<td id="summaryHtml"></td>
+				<td id="textHtml" colspan="2"></td>
+			<tr>
+				<td id="relatedGenes"></td>
+				<td id="symbol"></td>
+				<td id="relatedDrugs"></td>
+			</tr>
+			</tr>
+		</tbody>
+	</table>
+	<!-- <div id="resultado_gen"></div> -->
+</body>
+</html>
